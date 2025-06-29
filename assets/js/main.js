b479cef8f313de8cb14464bf12a66d0c20469bf9
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-29 18:54:30 +0300

    03. usefull-things-104-intro-clippath. Bg, images.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,18 @@
-
 gsap.from("h2 div", 1.5, {
     yPercent: 100,
     ease: "power4.inOut",
     stagger: {
-        amount: 0.5
-    }
+        amount: 0.5,
+    },
 });
 
 gsap.to("h2", 1.5, {
     clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
     ease: "power4.inOut",
     stagger: {
-        amount: 0.5
-    }
+        amount: 0.5,
+    },
 });
-
 
 document.addEventListener("DOMContentLoaded", function () {
     let overlay = document.querySelector(".overlay");
@@ -24,59 +22,40 @@ document.addEventListener("DOMContentLoaded", function () {
             yPercent: -100,
             ease: "power4.inOut",
             stagger: {
-                amount: 0.5
-            }
+                amount: 0.5,
+            },
         });
 
-        gsap.to("h2", 1.5, {
-            clipPath: "polygon(0% 85%, 100% 85%, 100% 100%, 0% 100%)",
-            ease: "power4.inOut",
-            stagger: {
-                amount: 0.5
-            }
-        }, 0);
+        gsap.to(
+            "h2",
+            1.5,
+            {
+                clipPath: "polygon(0% 85%, 100% 85%, 100% 100%, 0% 100%)",
+                ease: "power4.inOut",
+                stagger: {
+                    amount: 0.5,
+                },
+            },
+            0
+        );
 
         gsap.to(".overlay", 2, {
             clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-            ease: "power4.inOut"
+            ease: "power4.inOut",
         });
 
         gsap.to(".img", 2, {
             clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
             ease: "power4.inOut",
             stagger: {
-                amount: 1.5
-            }
+                amount: 1.5,
+            },
         });
 
         gsap.to(".loader", 2, {
             clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
             ease: "power4.inOut",
-            delay: 2
+            delay: 2,
         });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
